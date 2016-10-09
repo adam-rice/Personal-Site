d8cbfc0393db06e56abd3d7e638f281939d6e788
Adam Rice <adamcoltonrice@gmail.com>
2016-10-08 21:56:29 -0600

    scroll to section on nav bar functional

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ $(window).scroll(function() {
 if ($(this).scrollTop() > 1){
     $('header').addClass("sticky");
     $('#logo').addClass("sticky");
-    $('.nav').addClass("sticky");
+    $('.float-right').addClass("sticky");
   }
   else{
     $('header').removeClass("sticky");
     $('#logo').removeClass("sticky");
-    $('.nav').removeClass("sticky");
+    $('.float-right').removeClass("sticky");
   }
 });
+
+$("#header-links a").click(function(e){
+    e.preventDefault();
+    $('html, body').animate( {
+      scrollTop:$(this.hash).offset().top
+    },
+    'slow');
+  });
